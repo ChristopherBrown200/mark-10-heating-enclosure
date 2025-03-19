85194869ca02d55eec03bfa1d8d4bc13150b28fd
Christopher Brown <gb12cb1@gmail.com>
2025-03-18 22:23:32 -0400

    libraries and consts

--- mainSketch/mainSketch.ino
+++ mainSketch/mainSketch.ino
@@ -1,9 +1,41 @@
+// Included Libraries
+#include <AccelStepper.h>
+#include "Adafruit_MAX31855.h"
+#include <LiquidCrystal_I2C.h>
 
+// Thermocouple Digital IO Pins
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
 
+// Initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
+// Display Setup
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Stepper Motor Digital IO Pins
+const int dirPin = 2;
+const int stepPin = 3;
+
+// Define Motor Interface
+#define motorInterfaceType 1
+
+// Create Stepper Motor Instance
+AccelStepper myStepper(motorInterface, stepPin, dirPin);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Setup Display
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+
+  // Stepper Motor Setup
+  myStepper.setMaxSpeed(1000);
+  myStepper.setAcceleration(50);
+  myStepper.setSpeed(200);
+
+  setDisplay("Set Starting", "Height.");
 
 }
 
@@ -11,3 +43,31 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+void setDisplay(char line1[], char line2[])
+{
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
