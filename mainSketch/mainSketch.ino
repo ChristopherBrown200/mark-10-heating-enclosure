cee062e15c433cebb4b4af2ac44cdcf7ca743f5d
Christopher Brown <gb12cb1@gmail.com>
2025-03-19 17:55:42 -0400

    setup without moving

--- mainSketch/mainSketch.ino
+++ mainSketch/mainSketch.ino
@@ -14,7 +14,7 @@ Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 double actualTemp = 0.0;
 double goalTemp = 70.0;
-const double maxTempeture = 150.0; //TODO: Find Actual Max
+const double maxTemp = 150.0; //TODO: Find Actual Max
 
 
 // Display Setup
@@ -28,7 +28,12 @@ const int stepPin = 3;
 #define motorInterfaceType 1
 
 // Create Stepper Motor Instance
-AccelStepper myStepper(motorInterface, stepPin, dirPin);
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
+// Motor Varibles
+const double MaxSpeed = 70.0;
+double Speed = 10.0; // TODO: Set as min speed
+bool returnEnabled = false;
 
 // Define encoder Pins
 #define CLK_PIN 27
@@ -42,7 +47,10 @@ AccelStepper myStepper(motorInterface, stepPin, dirPin);
 // Encoder Varibles
 int direction = DIRECTION_CW;
 int CLK_state;
-int prev_CLK_state
+int prev_CLK_state;
+
+// Relay Pin
+#define RELAY_PIN 99
 
 
 // Boot squence and Pretest Setup ======================================================
@@ -50,6 +58,9 @@ void setup() {
   Serial.begin(9600);
 
   while (!Serial) delay(1);
+
+  // Relay Setup
+  pinMode(RELAY_PIN, OUTPUT);
 
   // Setup Display
   lcd.init();
@@ -65,20 +76,20 @@ void setup() {
   pinMode(CLK_PIN, INPUT);
   pinMode(DT_PIN, INPUT);
   pinMode(SW_PIN, INPUT);
-  prev_CLK_State = digitalRead(CLK_PIN);
+  prev_CLK_state = digitalRead(CLK_PIN);
 
   // Setup Thermocouple
   if (!thermocouple.begin()){
-    Serial.println("Thermocouple Error")
+    Serial.println("Thermocouple Error");
     while (1) delay(10);
   }
-  actualTemp = thermocouple.readInternal()
+  actualTemp = thermocouple.readCelsius();
 
   // Set the starting height
   setDisplay("Set Starting", "Height.");
   while (digitalRead(SW_PIN) == 1){
     CLK_state = digitalRead(CLK_PIN);
-    if (CLK_state !+ prev_CLK_State){
+    if (CLK_state != prev_CLK_state){
       if (digitalRead(DT_PIN) == HIGH){
         // Move UP
         direction = DIRECTION_CCW;
@@ -91,41 +102,102 @@ void setup() {
     }
   }
 
-  // Prompt the user to close the enclosure
-  setDisplay("Please ensure", "door is closed.")
-  while (digitalRead(SW_PIN) == 1) delay(1);
-
-  // Get the desired tempeture
-  setDisplay("Tempeture", "")
+  // Set the Expansion Rate
+  setDisplay("Set Expansion", "Speed:");
   while (digitalRead(SW_PIN) == 1){
     CLK_state = digitalRead(CLK_PIN);
-    if (CLK_state != prev_CLK_State){
+    if (CLK_state != prev_CLK_state){
       if (digitalRead(DT_PIN) == HIGH){
-        goalTemp += 1.0;
+        if (Speed < MaxSpeed){
+          Speed += 0.1;
+        }
         direction = DIRECTION_CCW;
       }
       else{
-        goalTemp -= 1.0;
+        if (Speed > 0){
+          Speed -= 0.1;
+        }
         direction = DIRECTION_CW;
       }
       prev_CLK_state = CLK_state;
-    }
 
-    // TODO: Display goalTemp
+      lcd.setCursor(1, 6);
+      lcd.print("          ");
+      lcd.setCursor(1, 6);
+      lcd.print(Speed);
+      lcd.print(" in/min");
+    }
+  }
+
+  setDisplay("Return Home?", "N");
+  while (digitalRead(SW_PIN) == 1){
+    CLK_state = digitalRead(CLK_PIN);
+    if (CLK_state != prev_CLK_state){
+      returnEnabled = !returnEnabled;
+      if (returnEnabled){
+        setDisplay("Return Home?", "Y");
+      }
+      else {
+        setDisplay("Return Home?", "N");
+      }
+    }
+  }
+
+  // Prompt the user to close the enclosure
+  setDisplay("Please ensure", "door is closed.");
+  while (digitalRead(SW_PIN) == 1) delay(1);
+
+  // Get the desired tempeture
+  setDisplay("Tempeture", "");
+  while (digitalRead(SW_PIN) == 1){
+    CLK_state = digitalRead(CLK_PIN);
+    if (CLK_state != prev_CLK_state){
+      if (digitalRead(DT_PIN) == HIGH){
+        if (goalTemp < maxTemp){
+          goalTemp += 1.0;
+        }
+        direction = DIRECTION_CCW;
+      }
+      else {
+        if (goalTemp > 0){
+          goalTemp -= 1.0;
+        }
+        direction = DIRECTION_CW;
+      }
+      prev_CLK_state = CLK_state;
+
+      lcd.setCursor(1, 0);
+      lcd.print(goalTemp);
+      lcd.print("C");
+    }
   }
 
   // Wait for enclosure to reach tempeture
   setDisplay("Heating", "Chamber");
+  digitalWrite(RELAY_PIN, HIGH);
   do{
-    actualTemp = thermocouple.readInternal();
+    actualTemp = thermocouple.readCelsius();
   } while (actualTemp < goalTemp);
+
+  setDisplay("Ready Press to", "Start");
+  while (digitalRead(SW_PIN) == 1) maintainTemp();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
 
+void maintainTemp(){
+  actualTemp = thermocouple.readCelsius();
+
+  if (actualTemp >= goalTemp + 2.5){
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+  else if (actualTemp < goalTemp - 1.0){
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
 
 void setDisplay(char line1[], char line2[])
 {
